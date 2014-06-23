d6045e6c6a20edbcd375dfa991beb4878c7c538d
XiongJian <420048248@qq.com>
2014-06-23 10:06:11 +0800

    Initial Commit

---
+++ QRcode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QRcode
+//
+//  Created by XiongJian on 14-6-23.
+//  Copyright (c) 2014年 Static. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QRcode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QRcode
+//
+//  Created by XiongJian on 14-6-23.
+//  Copyright (c) 2014年 Static. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
